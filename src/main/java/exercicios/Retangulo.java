b756ac9f110b2d3638fdcc60a44f8d7ecfca7ab6
any-gomes <any.gomes@outlook.com.br>
2020-02-19 20:43:23 -0300

    Criação da classe Retangulo

---
+++ src/main/java/exercicios/Retangulo.java
@@ -0,0 +1,16 @@
+package exercicios;
+
+public class Retangulo {
+    public Double base;
+    public Double altura;
+
+    public Double calcularArea() {
+
+        return base * altura;
+    }
+
+    public Double calcularPerimetro() {
+
+        return 2*(base + altura);
+    }
+}
